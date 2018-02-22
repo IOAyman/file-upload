cd9bc151406f9ce0e7555050728d9a820f34b4bd
Ayman Nedjmeddine <theycallmethedr@gmail.com>
2018-02-22 16:06:55 +0100

    update

--- go.js
+++ go.js
@@ -1,28 +1,30 @@
 const express = require('express')
 const cors = require('cors')
 const multer = require('multer')
-const path = require(('path'))
+const { join } = require('path')
 
 
 const app = express()
-app.set('port', 8080)
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, 'uploads'),
+  destination: join(__dirname, 'uploads'),
   filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
 })
 const upload = multer({ storage })
 
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+app.get('/', (req, res) => res.sendFile(join(__dirname, 'index.html')))
 
-app.post('/', upload.single('io'), (req, res) => {
-  res.end('thanks')
+app.post('/', upload.single('io'), (req, res) => res.end('thanks'))
+
+app.all('*', (req, res) => res.redirect('/'))
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.end('woops!')
 })
 
-
 app.on('error', console.error)
-app.listen(app.get('port'))
+app.listen(process.env.NODE_PORT || 9999)
